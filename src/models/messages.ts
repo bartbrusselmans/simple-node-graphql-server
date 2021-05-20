a4d8a71036b7b835c82a6081ba0d1f167723b041
Bart Brusselmans <bart.brusselmans@two-point-o.be>
2021-05-20 12:35:02 +0200

    setup models and types

---
+++ src/models/messages.ts
@@ -0,0 +1,16 @@
+import { IMessage } from '../types/message';
+
+const messages: IMessage[] = [
+  {
+    id: '1',
+    text: 'Hello world',
+    userId: '1',
+  },
+  {
+    id: '2',
+    text: 'By world',
+    userId: '2',
+  },
+];
+
+export default messages;
---
+++ src/models/users.ts
@@ -0,0 +1,16 @@
+import { IUser } from '../types/user';
+
+const users: IUser[] = [
+  {
+    id: '1',
+    messageIds: ['1'],
+    username: 'Bart Brusselmans',
+  },
+  {
+    id: '2',
+    messageIds: ['2'],
+    username: 'Joren Van Hee',
+  },
+];
+
+export default users;
